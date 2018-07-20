34debe971f9fc79e0f8ac29e39dcac288bd2246b
LepkoQQ <lepko.san@gmail.com>
2018-07-20 13:48:10 +0200

    update deps

--- run.cmd
+++ run.cmd
@@ -3,6 +3,6 @@
 set CMDER_START=%~dp0
 echo %CMDER_START%
 
-cmd /k "%ConEmuDir%\..\init.bat & npm start" -new_console:n:t:"npm watch"
+cmd /k "%CMDER_ROOT%\vendor\init.bat & npm start" -new_console:n:t:"npm watch"
 
-cmd /k "%ConEmuDir%\..\init.bat & npm run api" -new_console:s50H:n:t:"npm api"
+cmd /k "%CMDER_ROOT%\vendor\init.bat & npm run api" -new_console:s50H:n:t:"npm api"
