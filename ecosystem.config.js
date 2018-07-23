ac1fe32726bd9a0542b4bbe2fa12c0d383308fe9
LepkoQQ <lepko.san@gmail.com>
2018-07-23 12:16:24 +0200

    add deploy and migrate script and add ecosystem config for pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+const project = 'agrument';
+const command = [
+  'npm i && npm run build && npm run migrate',
+  'pm2 reload ecosystem.config.js',
+].join(' ');
+
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [{
+    name: project,
+    script: 'dist_server/server.js',
+    env: {
+      PORT: 8000,
+    },
+    env_production: {
+      PORT: 2000,
+    },
+  }],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'agrumentator',
+      host: 'agrument.djnd.si',
+      ref: 'origin/master',
+      repo: `https://github.com/danesjenovdan/${project}.git`,
+      path: `/home/agrumentator/${project}`,
+      'post-deploy': `${command} --env production --update-env`,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  },
+};
