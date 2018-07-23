489595379931153b1bc5fec76d496c09a45d8a8d
LepkoQQ <lepko.san@gmail.com>
2018-07-23 15:29:37 +0200

    remove temp link to dash

--- src/components/Agrument/Container.js
+++ src/components/Agrument/Container.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { StickyContainer, Sticky } from 'react-sticky';
 import Feed from './Feed';
 import Sidebar from './Sidebar';
 
 const Container = () => (
   <div className="container agrument__container">
-    <Link to="/dash">Dash</Link>
     <StickyContainer>
-      { /* TODO: remove this link */ }
       <div className="row">
         <div className="col-md-9">
           <Feed />
         </div>
         <div className="col-md-3 hidden-xs hidden-sm">
-          <Sticky>{
-            ({ style }) => (
+          <Sticky>
+            {({ style }) => (
               <div style={style}>
                 <Sidebar />
               </div>
-            )
-          }</Sticky>
+            )}
+          </Sticky>
         </div>
       </div>
     </StickyContainer>
