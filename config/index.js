68220504f0ac6a2bd2909460b861b966928bccba
LepkoQQ <lepko.san@gmail.com>
2018-08-24 15:58:24 +0200

    add some comments to config

--- config/index.js
+++ config/index.js
@@ -5,11 +5,15 @@ const config = {
   SLACK_WEBHOOK_URL: '',
   SESSION_SECRET: 'sekkrett',
   TWITTER_SECRET: '',
-  MEDIA_PATH: './media/',
-  MEDIA_URL: '/media/',
+  // These MEDIA_PATH and MEDIA_URL default values are what is served by express
+  // if you change them in your config make sure you configure an external server
+  // like nginx to serve the files from the corect folder and location.
+  MEDIA_PATH: './media/', // images are saved to this path
+  MEDIA_URL: '/media/', // images are served from this url
 };
 
 if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line import/no-unresolved
   module.exports = merge({}, config, require('./production'));
 } else {
   module.exports = config;
