a2d0646fbf00eb046108f2cd0245255456344821
LepkoQQ <lepko.san@gmail.com>
2018-08-24 15:58:49 +0200

    move auth setup to own file

--- src/server/index.js
+++ src/server/index.js
@@ -4,14 +4,7 @@
  */
 import path from 'path';
 import express from 'express';
-import session from 'express-session';
-import sessionKnex from 'connect-session-knex';
 import bodyParser from 'body-parser';
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import passwordHashAndSalt from 'password-hash-and-salt';
-import _ from 'lodash';
-import db from './database';
 import renderApp from './middleware/app';
 import agrumentRouter from './routes/agrument';
 import dashRouter from './routes/dashboard';
@@ -20,7 +13,7 @@ import apiRouter from './routes/api';
 import apiRouterV2 from './routes/api_v2';
 import feedRouter from './routes/feed';
 import { sendErrorToSlack, sendErrorToSlackMiddleware } from './slack';
-import config from '../../config';
+import useSessionAuth from './session-auth';
 
 process.on('uncaughtException', (err) => {
   // eslint-disable-next-line no-console
@@ -46,8 +39,6 @@ process.on('unhandledRejection', (reason, p) => {
   });
 });
 
-const KnexSessionStore = sessionKnex(session);
-
 const app = express();
 app.disable('x-powered-by');
 
@@ -63,82 +54,8 @@ app.use('/api/v2', apiRouterV2);
 app.use('/get', apiRouter);
 app.use('/rss', feedRouter);
 
-passport.use(new LocalStrategy((username, pass, done) => {
-  db('users')
-    .where('username', username)
-    .first()
-    .then((user) => {
-      if (user && !user.disabled) {
-        passwordHashAndSalt(pass).verifyAgainst(user.password, (error, verified) => {
-          if (error) {
-            throw new Error(error);
-          }
-          if (!verified) {
-            done(null, null);
-          } else {
-            done(null, {
-              id: user.id,
-              username: user.username,
-              password: user.password,
-            });
-          }
-        });
-      } else {
-        done(null, null);
-      }
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.log('failed to verify password hash', error);
-      done(error, null);
-    });
-}));
-
-passport.serializeUser((user, done) => {
-  // this is the data stored to the session
-  const sessionUser = _.pick(user, ['id', 'username', 'password']);
-  done(null, sessionUser);
-});
-
-passport.deserializeUser((sessionUser, done) => {
-  // hit the db here if session doesn't have all the data you need on the user
-  db('users')
-    .where('id', sessionUser.id)
-    .first()
-    .then((user) => {
-      if (user.disabled) {
-        // if user is disabled don't set req.user
-        done(null, null);
-      } else if (user.username !== sessionUser.username || user.password !== sessionUser.password) {
-        // if username or password hash don't match don't set req.user
-        done(null, null);
-      } else {
-        // this sets req.user
-        done(null, {
-          id: user.id,
-          name: user.name,
-          username: user.username,
-          group: user.group,
-          password: user.password,
-        });
-      }
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.log('failed to deserialize user', error);
-      done(error, null);
-    });
-});
-
-app.use(session({
-  saveUninitialized: false, // save new sessions only when modified (e.g. login sessions)
-  resave: false, // don't resave to store if not modified
-  store: new KnexSessionStore({ knex: db }),
-  secret: config.SESSION_SECRET,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
+// everything after this creates sessions
+useSessionAuth(app);
 
 app.use('/api', authRouter);
 
