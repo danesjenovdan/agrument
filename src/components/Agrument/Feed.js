167b66014cacec11cd79a8c0e529e0e46e0bc29b
LepkoQQ <lepko.san@gmail.com>
2019-07-01 16:58:25 +0200

    change cc0 snippet

--- src/components/Agrument/Feed.js
+++ src/components/Agrument/Feed.js
@@ -218,9 +218,18 @@ class Feed extends React.Component {
       content.push((
         <div key="in-feed-subscribe" className="hidden-md hidden-lg in-feed-subscribe">
           <SubscribeForm />
-          <div className="agrument__license">
-            Izdano pod licenco <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.sl" target="_blank" rel="noopener noreferrer">CC0</a>.
-          </div>
+          <div
+            className="agrument__license"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                <p xmlns:dct="http://purl.org/dc/terms/">
+                  Izdano pod licenco <a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/deed.sl">CC0</a>.
+                  <small>Kolikor dopušča zakonodaja, se <a rel="dct:publisher" href="https://danesjenovdan.si/"><span property="dct:title">Danes je nov dan, Inštitut za druga vprašanja</span></a> odpoveduje vsem avtorskim in sorodnim pravicam glede <span property="dct:title">spletnega mesta danesjenovdan.si</span> z izjemo vseh ikon in slik objavljenih na <a href="https://agrument.danesjenovdan.si">Agrumentu</a>, ki jih nismo ustvarili na inštitutu.</small>
+                </p>
+              `,
+            }}
+          />
         </div>
       ));
       content.push(...tail(articles));
