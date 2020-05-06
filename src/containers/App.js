0dcfc04a35448e9b2b40bb92886dc6de38ca273d
LepkoQQ <lepko.san@gmail.com>
2020-05-06 20:58:48 +0200

    remove unused feed components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import SideMenu from '../components/SideMenu';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Reset from '../pages/Reset';
@@ -45,12 +44,9 @@ class App extends React.Component {
   render() {
     const state = store.get();
     return (
-      <>
-        <SideMenu />
-        <Switch>
-          {routes.map((r) => <Route key={`route[${r.path}]`} path={r.path} render={() => <r.component state={state} />} />)}
-        </Switch>
-      </>
+      <Switch>
+        {routes.map((r) => <Route key={`route[${r.path}]`} path={r.path} render={() => <r.component state={state} />} />)}
+      </Switch>
     );
   }
 }
