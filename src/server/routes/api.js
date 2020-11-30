e9b1898c81a30af2ab8e0ad3e5c5319df7c29023
LepkoQQ <lepko.san@gmail.com>
2020-11-30 20:08:08 +0100

    add option to pass year in to word-count endpoint

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -20,8 +20,23 @@ router.get('/last-agrument', (req, res) => {
 });
 
 router.get('/word-count', (req, res) => {
-  db('posts')
-    .where('type', 'published')
+  let year = null;
+  if (req.query.year && req.query.year.length === 4) {
+    const parsedYear = parseInt(req.query.year, 10);
+    if (parsedYear && parsedYear > 999 && parsedYear <= 9999) {
+      year = parsedYear;
+    }
+  }
+
+  let query = db('posts').where('type', 'published');
+
+  if (year) {
+    const startOfYear = Date.UTC(year, 0, 1);
+    const endOfYear = Date.UTC(year + 1, 0, 1) - 1;
+    query = query.andWhereBetween('date', [startOfYear, endOfYear]);
+  }
+
+  query
     .select('content')
     .then((data) => {
       const allWords = data
