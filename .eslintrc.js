8159ef1f67a0eab2919fb8298b670ee302e343ad
LepkoQQ <lepko.san@gmail.com>
2022-03-10 14:52:49 +0100

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'react/prop-types': ['off'],
+    'react/jsx-props-no-spreading': ['off'],
+    'import/extensions': ['error', 'ignorePackages'],
+    'import/prefer-default-export': ['off'],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: false,
+        devDependencies: ['**/.eslintrc.js', '**/vite.config.js'],
+      },
+    ],
+  },
+};
